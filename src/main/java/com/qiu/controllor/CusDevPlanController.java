c88293180975e9740a244eee00ea11a494bea5ba
Qiu68 <2454651737@qq.com>
2022-10-06 15:53:30 +0800

    第二次提交

--- src/main/java/com/qiu/controllor/CusDevPlanController.java
+++ src/main/java/com/qiu/controllor/CusDevPlanController.java
@@ -1,11 +1,17 @@
 package com.qiu.controllor;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author qiu
  * @create 2022/10/6 15:20
  **/
 @Controller
+@RequestMapping("/cus_dev_plan")
 public class CusDevPlanController {
+    @RequestMapping("/index")
+    public String toIndex(){
+        return "cusDevPlan/cus_dev_plan";
+    }
 }
--- src/main/resources/views/cusDevPlan/cus_dev_plan.ftl
+++ src/main/resources/views/cusDevPlan/cus_dev_plan.ftl
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>客户开发计划管理</title>
-	<#include "../common.ftl">
+	<#include "common.ftl">
 </head>
 <body class="childrenBody">
 <form class="layui-form" >
